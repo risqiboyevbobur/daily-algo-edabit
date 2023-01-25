e8663151e991894d088aaff146a6f875d3dc0b07
risqiboyevbobur <brisqiboyev@gmail.com>
2023-01-25 18:29:35 +0500

    fw

---
+++ january/index.js
@@ -0,0 +1,86 @@
+// function reverseString(str) {
+//     var c = "";
+//     for (var i = str.length - 1; i >= 0; i--) {
+//         c += str[i];
+//     }
+//     return c ;
+// }
+// console.log(reverseString('hura'));
+
+// function rever(str){
+//     let c = ""
+//     for(let i = str.length -1; i >= 0; i--){
+//         c+= str[i]
+//     }
+//     return c
+// }
+// console.log(rever('hura'));
+
+// function news(str){
+//     let a = ""
+//     for(let i = str.length -1; i>= 0; i--){
+//         a+= str[i]
+//     }
+//     return a
+// }
+// console.log(news('hura'));
+
+// function news(str){
+//     let a = ""
+//     for(let i = str.length -1 ; i >= 0; i-- ){
+//         a+= str[i]
+//     }
+//     return a
+// }
+// console.log(news('bobur'));
+
+// function b(price, persent){
+//     return price-(price * persent)/100
+// }
+// console.log(b(100, 37)); ------------------- colculate present
+
+// function a(b){
+//     return b % 10
+// }
+// console.log(a(34));
+
+// function c(v){
+//    return v.toString().length
+// }
+// console.log(c(24100));
+
+// function myFunc(a, b) {
+//   let obj = {
+//     num1: a,
+//     num2: b,
+//     funcs(){
+//    return obj.num1 + obj.num2 % 2 === 0 ?  true : false 
+//     }
+// }
+// return obj.funcs()
+// }
+// console.log(myFunc());
+
+
+// function num(a, b){
+//    return Math.floor(b / a)
+// }
+// console.log(num(4,13));
+
+
+// function num(a, b, c, d){
+//     return Math.max(a + b + c) < d ? true : false
+// }
+// console.log(num(100, 200, 300, 900));
+
+
+//  function sum([a,b,c], [d,e,f]){
+// let total = sum.reduce((total, value) => total + value)
+// return total
+//  }
+//  console.log();
+
+
+// / let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// let total = nums.reduce((total, value) => total + value )
+// console.log(total);   // 45
